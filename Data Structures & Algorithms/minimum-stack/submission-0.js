555c9457d8597672503ef8e40c53dd06b02d897f
Tyler G <77600391+TylerG21@users.noreply.github.com>
2026-07-23 15:26:58 -0700

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.js
@@ -0,0 +1,49 @@
+class MinStack {
+    constructor() {
+        this.stack = [];
+        this.minStack = [];
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+        this.stack.push(val)
+
+        const valForMinStack = Math.min(val, this.minStack.length === 0 ? val : this.minStack[this.minStack.length - 1])
+    
+        this.minStack.push(valForMinStack)
+
+    }
+
+    /**
+     * @return {void}
+     */
+    pop() {
+
+        this.stack.pop()
+        this.minStack.pop()
+
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+
+        return this.stack[this.stack.length - 1]
+
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin() {
+
+        return this.minStack[this.minStack.length - 1]
+
+
+
+    }
+}
